1de1fbd472f74bb3abf9041b0a08c650e977af79
Alan Panayotov <alanpanayotov@gmail.com>
2020-05-12 12:49:38 +0100

    add client auth + leave event firing

--- src/client/modules/Room.ts
+++ src/client/modules/Room.ts
@@ -2,6 +2,19 @@ import * as io from 'socket.io-client';
 import { IRoom } from '../../server/modals/IRoom';
 const socket = io();
 
+// TODO: client user interface
+// TODO: put 'me' variable somewhere better
+export var me: any = {};
+
+socket.on('connect', () => {
+  socket.emit('auth')
+  socket.once('res', (user) => {
+    me = user
+    console.log('Got me: ', me)
+  })
+})
+
+
 export interface RoomConnectionOptions {
   user: any, //TODO: define user type
   roomID: string,
@@ -9,7 +22,8 @@ export interface RoomConnectionOptions {
 }
 
 export const subscribeToRoom = (options: RoomConnectionOptions, cb: any, err: any) => {
-  socket.emit('subscribeToRoom', { user: options.user, roomID: options.roomID, password: options.password }); // subscribe anonymously
+  // TODO: allow non-anonymous subscription
+  socket.emit('subscribeToRoom', { user: options.user, roomID: options.roomID, password: options.password });
   socket.on('roomState', cb);
 
   socket.once('err', err);
@@ -21,4 +35,8 @@ export const createRoom = (options: IRoom) => {
     socket.once('res', resolve);
     socket.once('err', reject);
   });
+}
+
+export const leaveRoom = () => {
+  socket.emit('leaveRoom')
 }
